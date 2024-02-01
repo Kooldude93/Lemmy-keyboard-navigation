6654207390baf5339b43125a4000bb9367ca25b0
Kooldude93 <orlandovv93@gmail.com>
2024-02-01 14:14:40 -0800

    Update script links

--- main.user.js
+++ main.user.js
@@ -8,8 +8,8 @@
 // @author        aglidden
 // @author        howdy-tsc
 // @license       GPL3
-// @require       https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/main/lemmy-keyboard-navigation.user.js#md5=00869c5d5a124367afc8bafb9db39e0d
-// @require       https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/main/lemmy-keyboard-navigation-mlmym.user.js#md5=c7542b4ba8de41cf87e1386791a6da7d
+// @require       https://github.com/Kooldude93/Lemmy-keyboard-navigation/raw/main/lemmy-keyboard-navigation.user.js
+// @require       https://github.com/Kooldude93/Lemmy-keyboard-navigation/raw/main/lemmy-keyboard-navigation-mlmym.user.js
 // @icon          https://raw.githubusercontent.com/vmavromatis/Lemmy-keyboard-navigation/main/icon.png?inline=true
 // @homepageURL   https://github.com/vmavromatis/Lemmy-keyboard-navigation
 // @namespace     https://github.com/vmavromatis/Lemmy-keyboard-navigation
